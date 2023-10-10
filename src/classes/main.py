29d22aaef10fe30c186f4554594dde1ebcb33178
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-10-10 09:18:00 +0530

    created classes to explain the purpose of this pointer in oops

--- src/classes/main.py
+++ src/classes/main.py
@@ -34,7 +34,7 @@ class Inventories:
         self.QuantityInStock = QuantityInStock
 
 # # Create instances of the classes
-# user1 = User("user123", "Admin", "admin_user", "admin_pass")
+user1 = User("user123", "Admin", "admin_user", "admin_pass")
 # prescription1 = Prescriptions("presc12", "2023-02-26", 150.0, "user123")
 # medicine1 = Medicines("med123", "Aspirin", "Appollo Pharma", 5.99, "2023-12-31")
 # inventory1 = Inventories("med123", 100)
@@ -49,31 +49,31 @@ class Inventories:
 # print(prescription_detail1.Dosage)  # Access the Quantity attribute of the PrescriptionDetails instance
 
 
-# Proof of concept for Array of objects
-# Create instances of User
-user1 = User(1, 'Admin', 'admin_user', 'admin_password')
-user2 = User(2, 'Doctor', 'doctor_user', 'doctor_password')
+# # Proof of concept for Array of objects
+# # Create instances of User
+# user1 = User(1, 'Admin', 'admin_user', 'admin_password')
+# user2 = User(2, 'Doctor', 'doctor_user', 'doctor_password')
 
-# Create instances of Medicines
-medicine1 = Medicines(101, 'Medicine A', 'Manufacturer X', 10.0, '2024-01-01')
-medicine2 = Medicines(102, 'Medicine B', 'Manufacturer Y', 8.0, '2023-12-31')
+# # Create instances of Medicines
+# medicine1 = Medicines(101, 'Medicine A', 'Manufacturer X', 10.0, '2024-01-01')
+# medicine2 = Medicines(102, 'Medicine B', 'Manufacturer Y', 8.0, '2023-12-31')
 
-# Create instances of Inventories
-inventory1 = Inventories(101, 100)
-inventory2 = Inventories(102, 50)
+# # Create instances of Inventories
+# inventory1 = Inventories(101, 100)
+# inventory2 = Inventories(102, 50)
 
 
-# Create instances of PrescriptionDetails
-prescription_details1 = PrescriptionDetails(1, 'Take after meals', 101, 2, '2 tablets/day')
-prescription_details2 = PrescriptionDetails(2, 'Take before bedtime', 102, 1, '1 tablet/day')
+# # Create instances of PrescriptionDetails
+# prescription_details1 = PrescriptionDetails(1, 'Take after meals', 101, 2, '2 tablets/day')
+# prescription_details2 = PrescriptionDetails(2, 'Take before bedtime', 102, 1, '1 tablet/day')
 
-# Create instances of Prescriptions and associate with User
-prescription1 = Prescriptions(101, '2023-10-10', 20.0, user1)
-prescription2 = Prescriptions(102, '2023-10-11', 8.0, user2)
+# # Create instances of Prescriptions and associate with User
+# prescription1 = Prescriptions(101, '2023-10-10', 20.0, user1)
+# prescription2 = Prescriptions(102, '2023-10-11', 8.0, user2)
 
-# Create an array (list) of Prescriptions instances
-prescriptions_array = [prescription1, prescription2]
+# # Create an array (list) of Prescriptions instances
+# prescriptions_array = [prescription1, prescription2]
 
-# Create an array (list) of PrescriptionDetails instances 
-prescription_details_array = [prescription_details1, prescription_details2]
+# # Create an array (list) of PrescriptionDetails instances 
+# prescription_details_array = [prescription_details1, prescription_details2]
 
